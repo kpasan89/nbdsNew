0abf842afd415c2e76148fcad9ff9845ff6723c7
pdhs <pdhs@pdhssp-ML350p>
2014-03-28 15:15:52 +0530

    Merge origin/master
    Conflicts:
    	README.md

--- src/java/gov/health/bean/AbstractionFormController.java
+++ src/java/gov/health/bean/AbstractionFormController.java
@@ -12,12 +12,14 @@ import gov.health.bean.*;
 import gov.health.entity.Area;
 import gov.health.facade.AbstractionFormFacade;
 import gov.health.entity.AbstractionForm;
+import gov.health.entity.Department;
 import gov.health.entity.DysmorphologyExamination;
 import gov.health.entity.Institution;
 import gov.health.entity.LabTest;
 import gov.health.entity.NotificationForm;
 import gov.health.entity.Person;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
@@ -51,6 +53,11 @@ public class AbstractionFormController implements Serializable {
     Institution institution;
     Area area;
     NotificationForm notificationForm;
+    Department department;
+    @Inject
+    DepartmentController departmentController;
+    @Inject
+    AreaController areaController;
 
 
     public String addNewAbstractionForm() {
@@ -89,6 +96,52 @@ public class AbstractionFormController implements Serializable {
         return "birth_diffect_abstraction_form";
     }
 
+    public List<Department> completeOfficialDepartments(String qry) {
+        System.out.println("Complete");
+        System.out.println("current = " + current);
+        if (current == null || current.getHospital() == null) {
+            System.out.println("Null");
+            return new ArrayList<Department>();
+        } else {
+            System.out.println("Hospital name" + current.getHospital().getName());
+            getDepartmentController().setInstitution(current.getHospital());
+            return getDepartmentController().completeInstitutionDepartments(qry);
+        }
+
+    }
+    
+     public List<Area> completeMohAreas(String qry) {
+        if (current == null || current.getDistrict()== null) {
+            return new ArrayList<Area>();
+        } else {
+            getAreaController().setSuperArea(current.getDistrict());
+            return getAreaController().completeAreasUnderSuperArea(qry);
+        }
+
+    }
+     
+          
+     public List<Area> completeGnAreas(String qry) {
+        if (current == null || current.getMohArea()== null) {
+            return new ArrayList<Area>();
+        } else {
+            getAreaController().setSuperArea(current.getMohArea());
+            return getAreaController().completeSkipedAreasUnderSuperArea(qry);
+        }
+     }
+     
+     
+     public List<Area> completeRdhsAreas(String qry) {
+        if (current == null || current.getDistrict()== null) {
+            return new ArrayList<Area>();
+        } else {
+            getAreaController().setSuperArea(current.getDistrict());
+            return getAreaController().completeAreasUnderSuperArea(qry);
+        }
+
+    }
+     
+    
     public Institution getInstitution() {
         return institution;
     }
@@ -142,6 +195,30 @@ public class AbstractionFormController implements Serializable {
         }
         JsfUtil.addSuccessMessage("Saved");
 
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public DepartmentController getDepartmentController() {
+        return departmentController;
+    }
+
+    public void setDepartmentController(DepartmentController departmentController) {
+        this.departmentController = departmentController;
+    }
+
+    public AreaController getAreaController() {
+        return areaController;
+    }
+
+    public void setAreaController(AreaController areaController) {
+        this.areaController = areaController;
     }
 
     public String getSelectText() {
